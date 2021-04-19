d808410779ea024c8cedec1efe678171ab504a38
Rafiur Rahman <rrfoundary@gmail.com>
2021-04-20 05:59:49 +0600

    Display Movies

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const movies = [
+  { _id: 2, name: "Puspa", rating: 7.6 },
+  { _id: 1, name: "Ek tha tiger", rating: 5.6 },
+  { _id: 4, name: "Lucky the racer", rating: 8.0 },
+  { _id: 5, name: "96", rating: 8.7 },
+  { _id: 10, name: "Chennai express", rating: 8.7 },
+  { _id: 7, name: "Kal ho na ho", rating: 9.1 },
+  { _id: 3, name: "RRR", rating: 6.6 },
+  { _id: 6, name: "Dangerous khiladi", rating: 8.61 },
+  { _id: 9, name: "Housefull iv", rating: 6.1 },
+  { _id: 8, name: "Good news", rating: 7.4 },
+];
+
+function displayMovie() {
+  const list = document.querySelector("#list");
+  movies.map((movie) => {
+    const listItem = document.createElement("tr");
+    listItem.innerHTML = `
+      <td>${movie._id}</td>
+      <td>${movie.name}</td>
+      <td>${movie.rating}</td>
+      <td><a href ="javascript:void(0)" onclick = 'deleteMovie(${movie._id})' class ="delete close">X</a></td>
+    `;
+    list.appendChild(listItem);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", displayMovie);
